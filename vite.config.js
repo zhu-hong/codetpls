fe33de9c9984fb4dce5dbc2ff409cd7efa9c4ba0
朱鸿 <zhxhyy@qq.com>
2024-12-02 17:45:16 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import unocss from 'unocss/vite'
+import { presetWind } from 'unocss'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    unocss({
+      presets: [
+        presetWind(),
+      ],
+    })
+  ],
+})

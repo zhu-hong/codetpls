4fb09017bf54052d5d9cde909faf10adf0a2638f
朱鸿 <zhxhyy@qq.com>
2024-12-05 14:57:20 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -55,43 +55,43 @@ function App() {
                 fontSize="24">产品标识卡</text>
 
               <text x='34' y='98' dominantBaseline="middle" textAnchor="middle" fontSize='14'>供应商</text>
-              <text x='68' y='101' fontSize='12'></text>
+              <text x='68' y='99' fontSize='12'></text>
 
               <text x='34' y='120' dominantBaseline="middle" textAnchor="middle" fontSize='14'>品名</text>
-              <text x='68' y='123' fontSize='12'></text>
+              <text x='68' y='121' fontSize='12'></text>
 
               <text x='34' y='143' dominantBaseline="middle" textAnchor="middle" fontSize='14'>规格</text>
-              <text x='68' y='146' fontSize='12'></text>
+              <text x='68' y='144' fontSize='12'></text>
 
               <text x='34' y='166' dominantBaseline="middle" textAnchor="middle" fontSize='14'>捆/炉号</text>
-              <text x='68' y='169' fontSize='12'></text>
+              <text x='68' y='167' fontSize='12'></text>
 
               <text x='34' y='189' dominantBaseline="middle" textAnchor="middle" fontSize='14'>数量</text>
-              <text x='68' y='192' fontSize='12'></text>
+              <text x='68' y='190' fontSize='12'></text>
 
               <text x='34' y='213' dominantBaseline="middle" textAnchor="middle" fontSize='14'>生产日期</text>
-              <text x='68' y='216' fontSize='12'></text>
+              <text x='68' y='214' fontSize='12'></text>
 
               <text x='296' y='98' dominantBaseline="middle" textAnchor="middle" fontSize='14'>采购单号</text>
-              <text x='340' y='101' fontSize='12'></text>
+              <text x='340' y='99' fontSize='12'></text>
 
               <text x='234' y='120' dominantBaseline="middle" textAnchor="middle" fontSize='14'>牌号</text>
-              <text x='264' y='123' fontSize='12'></text>
+              <text x='264' y='121' fontSize='12'></text>
 
               <text x='360' y='120' dominantBaseline="middle" textAnchor="middle" fontSize='14'>产地</text>
-              <text x='390' y='123' fontSize='12'></text>
+              <text x='390' y='121' fontSize='12'></text>
 
               <text x='200' y='166' dominantBaseline="middle" textAnchor="middle" fontSize='14'>批号</text>
-              <text x='234' y='169' fontSize='12'></text>
+              <text x='234' y='167' fontSize='12'></text>
 
               <text x='146' y='189' dominantBaseline="middle" textAnchor="middle" fontSize='14'>净重</text>
-              <text x='174' y='192' fontSize='12'></text>
+              <text x='174' y='190' fontSize='12'></text>
 
               <text x='250' y='189' dominantBaseline="middle" textAnchor="middle" fontSize='14'>毛重</text>
-              <text x='274' y='192' fontSize='12'></text>
+              <text x='274' y='190' fontSize='12'></text>
 
               <text x='200' y='213' dominantBaseline="middle" textAnchor="middle" fontSize='14'>检验员</text>
-              <text x='234' y='216' fontSize='12'></text>
+              <text x='234' y='214' fontSize='12'></text>
             </g>
           </svg>
         </div>
@@ -306,35 +306,163 @@ function App() {
 
               <text x="436" y="144" fontSize="15" textAnchor="end"></text>
 
-              <text x="20" y="211" fontSize="16">生产车间</text>
-              <text x="112" y="211" fontSize="16"></text>
-              <line x1="112" y1="215" x2="220" y2="215" stroke="#000000" strokeWidth="2" />
-              <text x="235" y="211" fontSize="16">班次</text>
-              <text x="294" y="211" fontSize="16"></text>
-              <line x1="294" y1="215" x2="406" y2="215" stroke="#000000" strokeWidth="2" />
+              <text x="20" y="186" fontSize="16">生产车间</text>
+              <text x="112" y="186" fontSize="16"></text>
+              <line x1="112" y1="190" x2="220" y2="190" stroke="#000000" strokeWidth="2" />
+              <text x="235" y="186" fontSize="16">班次</text>
+              <text x="294" y="186" fontSize="16"></text>
+              <line x1="294" y1="190" x2="406" y2="190" stroke="#000000" strokeWidth="2" />
 
-              <text x="20" y="261" fontSize="16">产品名称</text>
-              <text x="112" y="261" fontSize="16"></text>
-              <line x1="112" y1="265" x2="220" y2="265" stroke="#000000" strokeWidth="2" />
-              <text x="235" y="261" fontSize="16">图号</text>
-              <text x="294" y="261" fontSize="16"></text>
-              <line x1="294" y1="265" x2="406" y2="265" stroke="#000000" strokeWidth="2" />
+              <text x="20" y="236" fontSize="16">产品名称</text>
+              <text x="112" y="236" fontSize="16"></text>
+              <line x1="112" y1="240" x2="220" y2="240" stroke="#000000" strokeWidth="2" />
+              <text x="235" y="236" fontSize="16">图号</text>
+              <text x="294" y="236" fontSize="16"></text>
+              <line x1="294" y1="240" x2="406" y2="240" stroke="#000000" strokeWidth="2" />
 
-              <text x="20" y="311" fontSize="16">本道工序</text>
-              <text x="112" y="311" fontSize="16"></text>
-              <line x1="112" y1="315" x2="406" y2="315" stroke="#000000" strokeWidth="2" />
+              <text x="20" y="286" fontSize="16">本道工序</text>
+              <text x="112" y="286" fontSize="16"></text>
+              <line x1="112" y1="290" x2="406" y2="290" stroke="#000000" strokeWidth="2" />
 
-              <text x="20" y="361" fontSize="16">送检单位</text>
-              <text x="112" y="361" fontSize="16"></text>
-              <line x1="112" y1="365" x2="406" y2="365" stroke="#000000" strokeWidth="2" />
+              <text x="20" y="336" fontSize="16">送检单位</text>
+              <text x="112" y="336" fontSize="16"></text>
+              <line x1="112" y1="340" x2="406" y2="340" stroke="#000000" strokeWidth="2" />
 
-              <text x="20" y="411" fontSize="16">操作工</text>
-              <text x="112" y="411" fontSize="16"></text>
-              <line x1="112" y1="415" x2="406" y2="415" stroke="#000000" strokeWidth="2" />
+              <text x="20" y="386" fontSize="16">操作工</text>
+              <text x="112" y="386" fontSize="16"></text>
+              <line x1="112" y1="390" x2="406" y2="390" stroke="#000000" strokeWidth="2" />
 
-              <text x="20" y="461" fontSize="16">送检时间</text>
-              <text x="112" y="461" fontSize="16"></text>
-              <line x1="112" y1="465" x2="406" y2="465" stroke="#000000" strokeWidth="2" />
+              <text x="20" y="436" fontSize="16">送检时间</text>
+              <text x="112" y="436" fontSize="16"></text>
+              <line x1="112" y1="440" x2="406" y2="440" stroke="#000000" strokeWidth="2" />
+            </g>
+          </svg>
+        </div>
+
+        <h1>物料标识卡（14.7x10）</h1>
+
+        <div style={{ boxShadow: '0px 0px 20px 6px rgba(0,0,0,0.2)' }}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 450 306" shapeRendering="crispEdges" width="450" height="306">
+            <rect x="0" y="0" width="450" height="306" fill="#ffffff" />
+
+            <svg x="20" y="20" width="90" height="90" shapeRendering="crispEdges" xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 25 25">
+              <rect width="25" height="25" fill="#ffffff"></rect>
+              <path fill="#000000"
+                d="M0 0h7v1H0zM8 0h2v1H8zM11 0h1v1H11zM14 0h1v1H14zM18,0 h7v1H18zM0 1h1v1H0zM6 1h1v1H6zM8 1h2v1H8zM12 1h1v1H12zM16 1h1v1H16zM18 1h1v1H18zM24,1 h1v1H24zM0 2h1v1H0zM2 2h3v1H2zM6 2h1v1H6zM9 2h3v1H9zM14 2h3v1H14zM18 2h1v1H18zM20 2h3v1H20zM24,2 h1v1H24zM0 3h1v1H0zM2 3h3v1H2zM6 3h1v1H6zM8 3h1v1H8zM10 3h1v1H10zM18 3h1v1H18zM20 3h3v1H20zM24,3 h1v1H24zM0 4h1v1H0zM2 4h3v1H2zM6 4h1v1H6zM8 4h2v1H8zM12 4h1v1H12zM14 4h1v1H14zM18 4h1v1H18zM20 4h3v1H20zM24,4 h1v1H24zM0 5h1v1H0zM6 5h1v1H6zM8 5h1v1H8zM10 5h1v1H10zM12 5h2v1H12zM18 5h1v1H18zM24,5 h1v1H24zM0 6h7v1H0zM8 6h1v1H8zM10 6h1v1H10zM12 6h1v1H12zM14 6h1v1H14zM16 6h1v1H16zM18,6 h7v1H18zM9 7h1v1H9zM12 7h4v1H12zM3 8h1v1H3zM6 8h1v1H6zM9 8h2v1H9zM12 8h2v1H12zM15 8h1v1H15zM19 8h3v1H19zM23,8 h2v1H23zM4 9h1v1H4zM8 9h5v1H8zM17 9h1v1H17zM19 9h2v1H19zM22 9h1v1H22zM24,9 h1v1H24zM0 10h3v1H0zM5 10h3v1H5zM9 10h2v1H9zM13 10h1v1H13zM15 10h2v1H15zM18 10h3v1H18zM0 11h1v1H0zM2 11h3v1H2zM10 11h1v1H10zM16 11h1v1H16zM19 11h3v1H19zM0 12h2v1H0zM3 12h6v1H3zM11 12h2v1H11zM14 12h2v1H14zM18 12h1v1H18zM20 12h1v1H20zM22 12h1v1H22zM24,12 h1v1H24zM1 13h1v1H1zM3 13h2v1H3zM10 13h1v1H10zM15 13h3v1H15zM21 13h1v1H21zM23,13 h2v1H23zM0 14h1v1H0zM3 14h2v1H3zM6 14h3v1H6zM10 14h1v1H10zM14 14h1v1H14zM21 14h1v1H21zM23 14h1v1H23zM1 15h1v1H1zM5 15h1v1H5zM7 15h1v1H7zM10 15h4v1H10zM15 15h1v1H15zM17 15h3v1H17zM24,15 h1v1H24zM0 16h2v1H0zM3 16h2v1H3zM6 16h2v1H6zM9 16h2v1H9zM16 16h5v1H16zM24,16 h1v1H24zM8 17h1v1H8zM11 17h3v1H11zM15 17h2v1H15zM20 17h2v1H20zM23 17h1v1H23zM0 18h7v1H0zM9 18h2v1H9zM12 18h1v1H12zM14 18h1v1H14zM16 18h1v1H16zM18 18h1v1H18zM20 18h1v1H20zM22 18h2v1H22zM0 19h1v1H0zM6 19h1v1H6zM11 19h1v1H11zM13 19h2v1H13zM16 19h1v1H16zM20 19h1v1H20zM22,19 h3v1H22zM0 20h1v1H0zM2 20h3v1H2zM6 20h1v1H6zM10 20h1v1H10zM12 20h2v1H12zM15 20h6v1H15zM22 20h1v1H22zM24,20 h1v1H24zM0 21h1v1H0zM2 21h3v1H2zM6 21h1v1H6zM8 21h1v1H8zM11 21h2v1H11zM14 21h3v1H14zM18 21h2v1H18zM21 21h1v1H21zM23 21h1v1H23zM0 22h1v1H0zM2 22h3v1H2zM6 22h1v1H6zM9 22h1v1H9zM12 22h2v1H12zM15 22h2v1H15zM18 22h3v1H18zM24,22 h1v1H24zM0 23h1v1H0zM6 23h1v1H6zM9 23h2v1H9zM14 23h1v1H14zM19 23h3v1H19zM24,23 h1v1H24zM0 24h7v1H0zM9 24h1v1H9zM11 24h1v1H11zM16 24h3v1H16zM20 24h1v1H20zM22 24h1v1H22zM24,24 h1v1H24z">
+              </path>
+            </svg>
+
+            <g fontWeight="700" fontFamily="微软雅黑" fill="#000000">
+              <text fontWeight="800" x="225" y="64" dominantBaseline="middle" textAnchor="middle"
+                fontSize="32">物料标识卡</text>
+
+              <text x="24" y="164" fontSize="20">名称</text>
+              <text x="70" y="164" fontSize="20"></text>
+              <line x1="70" y1="168" x2="170" y2="168" stroke="#000000" strokeWidth="2" />
+              <text x="174" y="164" fontSize="20">图号</text>
+              <text x="220" y="164" fontSize="20"></text>
+              <line x1="220" y1="168" x2="350" y2="168" stroke="#000000" strokeWidth="2" />
+
+              <text x="24" y="224" fontSize="20">数量</text>
+              <text x="70" y="224" fontSize="20"></text>
+              <line x1="70" y1="228" x2="170" y2="228" stroke="#000000" strokeWidth="2" />
+              <text x="174" y="224" fontSize="20">批次</text>
+              <text x="220" y="224" fontSize="20"></text>
+              <line x1="220" y1="228" x2="350" y2="228" stroke="#000000" strokeWidth="2" />
+
+              <text x="24" y="284" fontSize="20">供应商</text>
+              <text x="90" y="284" fontSize="20"></text>
+              <line x1="90" y1="288" x2="170" y2="288" stroke="#000000" strokeWidth="2" />
+              <text x="174" y="284" fontSize="20">生产日期</text>
+              <text x="258" y="284" fontSize="20"></text>
+              <line x1="258" y1="288" x2="350" y2="288" stroke="#000000" strokeWidth="2" />
+            </g>
+          </svg>
+        </div>
+
+        <h1>工序流转卡（14.7x10）</h1>
+
+        <div style={{ boxShadow: '0px 0px 20px 6px rgba(0,0,0,0.2)' }}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 450 306" shapeRendering="crispEdges" width="450" height="306">
+            <rect x="0" y="0" width="450" height="306" fill="#ffffff" />
+
+            <svg x="24" y="14" width="37" height="37" shapeRendering="crispEdges" xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 25 25">
+              <rect width="25" height="25" fill="#ffffff"></rect>
+              <path fill="#000000"
+                d="M0 0h7v1H0zM8 0h2v1H8zM11 0h1v1H11zM14 0h1v1H14zM18,0 h7v1H18zM0 1h1v1H0zM6 1h1v1H6zM8 1h2v1H8zM12 1h1v1H12zM16 1h1v1H16zM18 1h1v1H18zM24,1 h1v1H24zM0 2h1v1H0zM2 2h3v1H2zM6 2h1v1H6zM9 2h3v1H9zM14 2h3v1H14zM18 2h1v1H18zM20 2h3v1H20zM24,2 h1v1H24zM0 3h1v1H0zM2 3h3v1H2zM6 3h1v1H6zM8 3h1v1H8zM10 3h1v1H10zM18 3h1v1H18zM20 3h3v1H20zM24,3 h1v1H24zM0 4h1v1H0zM2 4h3v1H2zM6 4h1v1H6zM8 4h2v1H8zM12 4h1v1H12zM14 4h1v1H14zM18 4h1v1H18zM20 4h3v1H20zM24,4 h1v1H24zM0 5h1v1H0zM6 5h1v1H6zM8 5h1v1H8zM10 5h1v1H10zM12 5h2v1H12zM18 5h1v1H18zM24,5 h1v1H24zM0 6h7v1H0zM8 6h1v1H8zM10 6h1v1H10zM12 6h1v1H12zM14 6h1v1H14zM16 6h1v1H16zM18,6 h7v1H18zM9 7h1v1H9zM12 7h4v1H12zM3 8h1v1H3zM6 8h1v1H6zM9 8h2v1H9zM12 8h2v1H12zM15 8h1v1H15zM19 8h3v1H19zM23,8 h2v1H23zM4 9h1v1H4zM8 9h5v1H8zM17 9h1v1H17zM19 9h2v1H19zM22 9h1v1H22zM24,9 h1v1H24zM0 10h3v1H0zM5 10h3v1H5zM9 10h2v1H9zM13 10h1v1H13zM15 10h2v1H15zM18 10h3v1H18zM0 11h1v1H0zM2 11h3v1H2zM10 11h1v1H10zM16 11h1v1H16zM19 11h3v1H19zM0 12h2v1H0zM3 12h6v1H3zM11 12h2v1H11zM14 12h2v1H14zM18 12h1v1H18zM20 12h1v1H20zM22 12h1v1H22zM24,12 h1v1H24zM1 13h1v1H1zM3 13h2v1H3zM10 13h1v1H10zM15 13h3v1H15zM21 13h1v1H21zM23,13 h2v1H23zM0 14h1v1H0zM3 14h2v1H3zM6 14h3v1H6zM10 14h1v1H10zM14 14h1v1H14zM21 14h1v1H21zM23 14h1v1H23zM1 15h1v1H1zM5 15h1v1H5zM7 15h1v1H7zM10 15h4v1H10zM15 15h1v1H15zM17 15h3v1H17zM24,15 h1v1H24zM0 16h2v1H0zM3 16h2v1H3zM6 16h2v1H6zM9 16h2v1H9zM16 16h5v1H16zM24,16 h1v1H24zM8 17h1v1H8zM11 17h3v1H11zM15 17h2v1H15zM20 17h2v1H20zM23 17h1v1H23zM0 18h7v1H0zM9 18h2v1H9zM12 18h1v1H12zM14 18h1v1H14zM16 18h1v1H16zM18 18h1v1H18zM20 18h1v1H20zM22 18h2v1H22zM0 19h1v1H0zM6 19h1v1H6zM11 19h1v1H11zM13 19h2v1H13zM16 19h1v1H16zM20 19h1v1H20zM22,19 h3v1H22zM0 20h1v1H0zM2 20h3v1H2zM6 20h1v1H6zM10 20h1v1H10zM12 20h2v1H12zM15 20h6v1H15zM22 20h1v1H22zM24,20 h1v1H24zM0 21h1v1H0zM2 21h3v1H2zM6 21h1v1H6zM8 21h1v1H8zM11 21h2v1H11zM14 21h3v1H14zM18 21h2v1H18zM21 21h1v1H21zM23 21h1v1H23zM0 22h1v1H0zM2 22h3v1H2zM6 22h1v1H6zM9 22h1v1H9zM12 22h2v1H12zM15 22h2v1H15zM18 22h3v1H18zM24,22 h1v1H24zM0 23h1v1H0zM6 23h1v1H6zM9 23h2v1H9zM14 23h1v1H14zM19 23h3v1H19zM24,23 h1v1H24zM0 24h7v1H0zM9 24h1v1H9zM11 24h1v1H11zM16 24h3v1H16zM20 24h1v1H20zM22 24h1v1H22zM24,24 h1v1H24z">
+              </path>
+            </svg>
+
+            <g fontWeight="700" fontFamily="微软雅黑" fill="#000000">
+              <text fontWeight="800" x="225" y="32" dominantBaseline="middle" textAnchor="middle" fontSize="13">建新赵氏科技有限公司减震骨架生产工序流转卡</text>
+
+              <rect x="14" y="58" width="422" height="217" fill="#ffffff" stroke='#000000' strokeWidth='1' />
+
+              <line x1="14" y1="82" x2="436" y2="82" stroke="#000000" strokeWidth="1" />
+              <line x1="14" y1="106" x2="436" y2="106" stroke="#000000" strokeWidth="1" />
+              <line x1="14" y1="130" x2="436" y2="130" stroke="#000000" strokeWidth="1" />
+              <line x1="14" y1="154" x2="436" y2="154" stroke="#000000" strokeWidth="1" />
+              <line x1="14" y1="178" x2="436" y2="178" stroke="#000000" strokeWidth="1" />
+              <line x1="14" y1="202" x2="436" y2="202" stroke="#000000" strokeWidth="1" />
+              <line x1="14" y1="226" x2="436" y2="226" stroke="#000000" strokeWidth="1" />
+              <line x1="14" y1="250" x2="436" y2="250" stroke="#000000" strokeWidth="1" />
+
+              <line x1="64" y1="58" x2="64" y2="275" stroke="#000000" strokeWidth="1" />
+              <line x1="158" y1="58" x2="158" y2="275" stroke="#000000" strokeWidth="1" />
+              <line x1="208" y1="58" x2="208" y2="275" stroke="#000000" strokeWidth="1" />
+              <line x1="258" y1="106" x2="258" y2="275" stroke="#000000" strokeWidth="1" />
+              <line x1="308" y1="58" x2="308" y2="275" stroke="#000000" strokeWidth="1" />
+              <line x1="360" y1="58" x2="360" y2="275" stroke="#000000" strokeWidth="1" />
+
+              <text x='38' y='74' dominantBaseline="middle" textAnchor="middle" fontSize='8'>日期</text>
+              <text x='112' y='74' dominantBaseline="middle" textAnchor="middle" fontSize='8'></text>
+
+              <text x='38' y='98' dominantBaseline="middle" textAnchor="middle" fontSize='8'>生产批次</text>
+              <text x='112' y='98' dominantBaseline="middle" textAnchor="middle" fontSize='8'></text>
+
+              <text x='183' y='74' dominantBaseline="middle" textAnchor="middle" fontSize='8'>物料编码</text>
+              <text x='258' y='74' dominantBaseline="middle" textAnchor="middle" fontSize='8'></text>
+
+              <text x='183' y='98' dominantBaseline="middle" textAnchor="middle" fontSize='8'>数量</text>
+              <text x='258' y='98' dominantBaseline="middle" textAnchor="middle" fontSize='8'></text>
+
+              <text x='334' y='74' dominantBaseline="middle" textAnchor="middle" fontSize='8'>物料名称</text>
+              <text x='398' y='74' dominantBaseline="middle" textAnchor="middle" fontSize='8'></text>
+
+              <text x='334' y='98' dominantBaseline="middle" textAnchor="middle" fontSize='8'>产品图号</text>
+              <text x='398' y='98' dominantBaseline="middle" textAnchor="middle" fontSize='8'></text>
+
+              <text x='112' y='122' dominantBaseline="middle" textAnchor="middle" fontSize='8'>流转工序</text>
+              <text x='182' y='122' dominantBaseline="middle" textAnchor="middle" fontSize='8'>订单号</text>
+              <text x='232' y='122' dominantBaseline="middle" textAnchor="middle" fontSize='8'>班次</text>
+              <text x='282' y='122' dominantBaseline="middle" textAnchor="middle" fontSize='8'>生产时间</text>
+              <text x='334' y='122' dominantBaseline="middle" textAnchor="middle" fontSize='8'>工序完成时间</text>
+              <text x='398' y='122' dominantBaseline="middle" textAnchor="middle" fontSize='8'>操作人员</text>
+
+              <text x='38' y='122' dominantBaseline="middle" textAnchor="middle" fontSize='8'>序号</text>
+
+              <text x='38' y='146' dominantBaseline="middle" textAnchor="middle" fontSize='8'>1</text>
+              <text x='112' y='146' dominantBaseline="middle" textAnchor="middle" fontSize='8'></text>
+
+              <text x='38' y='170' dominantBaseline="middle" textAnchor="middle" fontSize='8'>2</text>
+              <text x='112' y='170' dominantBaseline="middle" textAnchor="middle" fontSize='8'></text>
+
+              <text x='38' y='194' dominantBaseline="middle" textAnchor="middle" fontSize='8'>3</text>
+              <text x='112' y='194' dominantBaseline="middle" textAnchor="middle" fontSize='8'></text>
+
+              <text x='38' y='218' dominantBaseline="middle" textAnchor="middle" fontSize='8'>4</text>
+              <text x='112' y='218' dominantBaseline="middle" textAnchor="middle" fontSize='8'></text>
+
+              <text x='38' y='242' dominantBaseline="middle" textAnchor="middle" fontSize='8'>5</text>
+              <text x='112' y='242' dominantBaseline="middle" textAnchor="middle" fontSize='8'></text>
+
+              <text x='38' y='266' dominantBaseline="middle" textAnchor="middle" fontSize='8'>6</text>
+              <text x='112' y='266' dominantBaseline="middle" textAnchor="middle" fontSize='8'></text>
+
+              <text x="20" y="292" fontSize="7">送货单位/送货人：</text>
+              <text x="250" y="292" fontSize="7">送货单位/送货人：</text>
             </g>
           </svg>
         </div>

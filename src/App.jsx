be487faab3af956e6302a6f3f5e487a2b2afadc4
朱鸿 <zhxhyy@qq.com>
2025-06-13 13:04:55 +0800

    SMT车间巡检

--- src/App.jsx
+++ src/App.jsx
@@ -59,42 +59,36 @@ function App() {
     }
 
     const data = [
-      ['SMT首件确认记录表', null, null, null, null, null, null, null, null],
-      ['订单号', xlsxData.saleCode, '产品型号', xlsxData.materialModel, '线别', xlsxData.workshopName, '班别', xlsxData.lineName, '日期', xlsxData.textTime],
-      ['工序', '检查项目', null, null, null, '检查结果(OK/NG)', null, null, '不良情形/处理状况', null],
-      [null, null, null, null, null, '1', '2', '3', null, null],
-      ...xlsxData.beforeTheFurnaceVOList.map((b, i) => ([
-        i === 0 ? '炉前' : null,
-        `${i + 1}、` + b.checkProjectName + '____________。',
-        null,
-        null,
-        null,
-        ...b.checkResultList.slice(0, 3).map((r) => getResultText(r)),
-        null,
-        null,
-      ])),
-      ...xlsxData.aoiVoList.map((b, i) => ([
-        i === 0 ? 'AOI' : null,
-        `${i + 1}、` + b.checkProjectName + '____________。',
-        null,
-        null,
-        null,
-        ...b.checkResultList.slice(0, 3).map((r) => getResultText(r)),
-        null,
-        null,
-      ])),
-      ...xlsxData.functionTestVOList.map((b, i) => ([
-        i === 0 ? '功能测试' : null,
-        `${i + 1}、` + b.checkProjectName + '____________。',
-        null,
-        null,
-        null,
-        ...b.checkResultList.slice(0, 3).map((r) => getResultText(r)),
-        null,
-        null,
-      ])),
-      [`炉前自检：${getResultText(xlsxData.beforeTheFurnaceCheck)}          技术员确认：${getResultText(xlsxData.technicianConfirm)}          AOI自检：${getResultText(xlsxData.aoiCheck)}           QC确认：${getResultText(xlsxData.qcConfirm)}         。`, null, null, null, null, null, null, null, null,],
-      [`    审核：${getResultText(xlsxData.audit)}                   日期：${xlsxData.textTime || ''}                          表单编号：${xlsxData.fromNo}   ${xlsxData.fromVersion}`, null, null, null, null, null, null, null, null,],
+      ['SMT巡检记录表', null, null, null, null, null, null, null, null, null, null, null, null],
+      [`订单号：${xlsxData.saleCode || ''}`, null, `规格型号：${xlsxData.materialModel || ''}`, null, `组别：${xlsxData.processType || ''}`, null, `日期：${xlsxData.endTime || ''}`, null, `仪器编号：${xlsxData.deviceNumber || ''}`, null, null, null, null],
+      ['类别', '巡检项目', null, null, null, null, '巡线时间', null, null, null, null, null, null],
+      [null, null, null, null, null, null, '', '', '', '', '', '', ''],
+      ['锡膏印刷', '锡膏是否先进先出原则领用，是否在室温下解冻4H，是否填写使用时间，领用及开盖后在24H内使用', null, null, null, null, '', '', '', '', '', '', ''],
+      [null, '印锡膏之PCB板上未贴片不得在线上停留超过60分钟', null, null, null, null, '', '', '', '', '', '', ''],
+      [null, '使用前是否有搅拌3分钟，并按少量多次原则添加使用', null, null, null, null, '', '', '', '', '', '', ''],
+      ['贴片', '"生产中途更换物料，调机优化必须经过组长、工程师、质控QC三方面先后确认后方可', null, null, null, null, '', '', '', '', '', '', ''],
+      [null, '工作台面是否整洁、物品摆放是否整齐、散料是否用静电袋（盒）存放', null, null, null, null, '', '', '', '', '', '', ''],
+      [null, '上板机、印刷机、贴片机、AOI日、周、月点检保养记录是否按时完成', null, null, null, null, '', '', '', '', '', '', ''],
+      [null, '在线站位表、工艺指导附件及贴片程序名是否与所生产机型名称一致', null, null, null, null, '', '', '', '', '', '', ''],
+      [null, '作业员拿板是否戴有静电环或静电手套', null, null, null, null, '', '', '', '', '', '', ''],
+      ['回流焊', '回流焊炉温及链速设置是否符合作业指导书及其附件的参数设定要求', null, null, null, null, '', '', '', '', '', '', ''],
+      [null, '生产检验员是否每小时进行功能测试并做好记录报表', null, null, null, null, '', '', '', '', '', '', ''],
+      ['AOI自动检测', '"经AOI检测的产品是否符合《贴片焊接检查判定标准》，AOI是否能正确的检测出良品不良品', null, null, null, null, '', '', '', '', '', '', ''],
+      [null, '检测出的良品、不良品有无标识且区分放置', null, null, null, null, '', '', '', '', '', '', ''],
+      [null, '作业员拿取PCB是否戴静电环，且轻拿轻放、不可推放、摔板、叠板', null, null, null, null, '', '', '', '', '', '', ''],
+      ['功能测试', '实际测试功能是否作业要求', null, '静耗', '', null, '', '', '', '', '', '', ''],
+      [null, null, null, '工作电流', '', null, '', '', '', '', '', '', ''],
+      [null, null, null, '充电电流', '', null, '', '', '', '', '', '', ''],
+      [null, '包装摆放是否符合要求', null, null, null, null, '', '', '', '', '', '', ''],
+      [`巡线结束时间：${xlsxData.endTime || ''}`, null, null, null, null, null, '', '', '', '', '', '', ''],
+      [`职位：${xlsxData.posts || ''}`, null, null, null, null, null, '', '', '', '', '', '', ''],
+      [`姓名（签字）：${xlsxData.textMan || ''}`, null, null, null, null, null, '', '', '', '', '', '', ''],
+      [`异常处理记录：`, null, null, null, null, null, null, null, null, null, null, null, null],
+      [`"注：
+ 1、每个检验项目只填发现不合格数和不良现象； 2、当抽样超标后IPQC应在异常处理记录中记录处理过程；
+     3、每个检验项目抽检3-5PCS；4、功能测试填写实测值。5、检验项不适用打“/”"												`, null, null, null, null, null, null, null, null, null, null, null, null],
+      [`巡检：${getResultText(xlsxData.ultimatelyCheckResult)}                                       审核：                            												`, null, null, null, null, null, null, null, null, null, null, null, null],
+      [`表单编号：SG-QR-211  版本/次：A/1												`, null, null, null, null, null, null, null, null, null, null, null, null],
     ]
     const workSheet = xlsx.utils.json_to_sheet(data, {
       skipHeader: true,
@@ -131,76 +125,126 @@ function App() {
       {
         wpx: 96,
       },
+      {
+        wpx: 96,
+      },
+      {
+        wpx: 96,
+      },
+      {
+        wpx: 96,
+      },
     ]
     workSheet['!rows'] = [
       {
         hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 86,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
       },
       {
-        hpx: 24,
+        hpx: 36,
+      },
+      {
+        hpx: 56,
+      },
+      {
+        hpx: 36,
+      },
+      {
+        hpx: 36,
+      },
+      {
+        hpx: 36,
+      },
+      {
+        hpx: 96,
+      },
+      {
+        hpx: 36,
+      },
+      {
+        hpx: 36,
       },
     ]
     workSheet['!merges'] = [
       {
         s: { r: 0, c: 0 },
-        e: { r: 0, c: 9 },
+        e: { r: 0, c: 12 },
+      },
+      {
+        s: { r: 1, c: 0 },
+        e: { r: 1, c: 1 },
+      },
+      {
+        s: { r: 1, c: 2 },
+        e: { r: 1, c: 3 },
+      },
+      {
+        s: { r: 1, c: 4 },
+        e: { r: 1, c: 5 },
+      },
+      {
+        s: { r: 1, c: 6 },
+        e: { r: 1, c: 7 },
+      },
+      {
+        s: { r: 1, c: 8 },
+        e: { r: 1, c: 12 },
       },
       {
         s: { r: 2, c: 0 },
@@ -208,109 +252,137 @@ function App() {
       },
       {
         s: { r: 2, c: 1 },
-        e: { r: 3, c: 4 },
+        e: { r: 3, c: 5 },
       },
       {
-        s: { r: 2, c: 5 },
-        e: { r: 2, c: 7 },
-      },
-      {
-        s: { r: 2, c: 8 },
-        e: { r: 3, c: 9 },
+        s: { r: 2, c: 6 },
+        e: { r: 2, c: 12 },
       },
       {
         s: { r: 4, c: 0 },
-        e: { r: 8, c: 0 },
-      },
-      {
-        s: { r: 9, c: 0 },
-        e: { r: 14, c: 0 },
-      },
-      {
-        s: { r: 15, c: 0 },
-        e: { r: 18, c: 0 },
+        e: { r: 6, c: 0 },
       },
       {
         s: { r: 4, c: 1 },
-        e: { r: 4, c: 4 },
+        e: { r: 4, c: 5 },
       },
       {
         s: { r: 5, c: 1 },
-        e: { r: 5, c: 4 },
+        e: { r: 5, c: 5 },
       },
       {
         s: { r: 6, c: 1 },
-        e: { r: 6, c: 4 },
+        e: { r: 6, c: 5 },
+      },
+      {
+        s: { r: 7, c: 0 },
+        e: { r: 11, c: 0 },
       },
       {
         s: { r: 7, c: 1 },
-        e: { r: 7, c: 4 },
+        e: { r: 7, c: 5 },
       },
       {
         s: { r: 8, c: 1 },
-        e: { r: 8, c: 4 },
+        e: { r: 8, c: 5 },
       },
       {
         s: { r: 9, c: 1 },
-        e: { r: 9, c: 4 },
+        e: { r: 9, c: 5 },
       },
       {
         s: { r: 10, c: 1 },
-        e: { r: 10, c: 4 },
+        e: { r: 10, c: 5 },
       },
       {
         s: { r: 11, c: 1 },
-        e: { r: 11, c: 4 },
+        e: { r: 11, c: 5 },
+      },
+      {
+        s: { r: 12, c: 0 },
+        e: { r: 13, c: 0 },
       },
       {
         s: { r: 12, c: 1 },
-        e: { r: 12, c: 4 },
+        e: { r: 12, c: 5 },
       },
       {
         s: { r: 13, c: 1 },
-        e: { r: 13, c: 4 },
+        e: { r: 13, c: 5 },
+      },
+      {
+        s: { r: 14, c: 0 },
+        e: { r: 16, c: 0 },
       },
       {
         s: { r: 14, c: 1 },
-        e: { r: 14, c: 4 },
+        e: { r: 14, c: 5 },
       },
       {
         s: { r: 15, c: 1 },
-        e: { r: 15, c: 4 },
+        e: { r: 15, c: 5 },
       },
       {
         s: { r: 16, c: 1 },
-        e: { r: 16, c: 4 },
+        e: { r: 16, c: 5 },
+      },
+      {
+        s: { r: 17, c: 0 },
+        e: { r: 20, c: 0 },
       },
       {
         s: { r: 17, c: 1 },
-        e: { r: 17, c: 4 },
+        e: { r: 19, c: 2 },
       },
       {
-        s: { r: 18, c: 1 },
-        e: { r: 18, c: 4 },
+        s: { r: 17, c: 4 },
+        e: { r: 17, c: 5 },
       },
       {
-        s: { r: 4, c: 8 },
-        e: { r: 18, c: 9 },
+        s: { r: 18, c: 4 },
+        e: { r: 18, c: 5 },
       },
       {
-        s: { r: 4, c: 8 },
-        e: { r: 18, c: 9 },
+        s: { r: 19, c: 4 },
+        e: { r: 19, c: 5 },
       },
       {
-        s: { r: 19, c: 0 },
-        e: { r: 19, c: 9 },
+        s: { r: 20, c: 1 },
+        e: { r: 20, c: 5 },
       },
       {
-        s: { r: 20, c: 0 },
-        e: { r: 20, c: 9 },
+        s: { r: 21, c: 0 },
+        e: { r: 21, c: 5 },
+      },
+      {
+        s: { r: 22, c: 0 },
+        e: { r: 22, c: 5 },
+      },
+      {
+        s: { r: 23, c: 0 },
+        e: { r: 23, c: 5 },
+      },
+      {
+        s: { r: 24, c: 0 },
+        e: { r: 24, c: 12 },
+      },
+      {
+        s: { r: 25, c: 0 },
+        e: { r: 25, c: 12 },
+      },
+      {
+        s: { r: 26, c: 0 },
+        e: { r: 26, c: 12 },
+      },
+      {
+        s: { r: 27, c: 0 },
+        e: { r: 27, c: 12 },
       },
     ]
 
-    Array.from({ length: 21 }).forEach((_, i) => {
+    Array.from({ length: 28 }).forEach((_, i) => {
       const idx = i + 1
-      'abcdefghij'.split('').map((l) => l.toUpperCase()).forEach((l) => {
+      'abcdefghijklm'.split('').map((l) => l.toUpperCase()).forEach((l) => {
         if(workSheet[l+idx]) {
           workSheet[l+idx].s = {
             font: {
@@ -319,35 +391,12 @@ function App() {
             },
             alignment: {
               vertical: 'center',
-              horizontal: 'left',
+              horizontal: 'center',
+              wrapText: 1,
             },
           }
         }
       })
-    })
-    workSheet['A1'].s = {
-      font: {
-        sz: 22,
-        name: '宋体',
-        bold: true,
-      },
-      alignment: {
-        vertical: 'center',
-        horizontal: 'center',
-      },
-    }
-    ;(['A3', 'B3', 'F3', 'F4', 'G4', 'H4', 'I3', 'A5', 'A10', 'A16']).forEach((c) => {
-      workSheet[c].s = {
-        font: {
-          sz: 14,
-          name: '宋体',
-          bold: true,
-        },
-        alignment: {
-          vertical: 'center',
-          horizontal: 'center',
-        },
-      }
     })
 
     xlsx.utils.book_append_sheet(workBook, workSheet, 'sheet')
